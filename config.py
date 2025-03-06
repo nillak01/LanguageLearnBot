24860b342886e5a94dab30a407d9756a4146672e
Kalinkin Yaroslav <kalinkin.y.o@mail.ru>
2025-03-07 00:06:17 +0300

    First work bot version

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+from environs import Env
+
+env = Env()  # Создаем экземпляр класса Env
+env.read_env()  # Методом read_env() читаем файл .env и загружаем из него переменные в окружение
+
+BOT_TOKEN = env('BOT_TOKEN')  # Получаем и сохраняем значение переменной окружения в переменную bot_token
+admin_id = env.int('ADMIN_ID')  # Получаем и преобразуем значение переменной окружения к типу int,
+                                # затем сохраняем в переменной admin_id
+
+# Выводим значения переменных в терминал
+# print(BOT_TOKEN)
+# print(admin_id)
+# print()
+
+# Убедимся также, что переменные есть в окружении
+# print(os.getenv('BOT_TOKEN'))
+# print(os.getenv('ADMIN_ID'))
